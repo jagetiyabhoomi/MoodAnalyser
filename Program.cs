0478215b4c50accf3200a0fcce8289407b4df98f
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-27 13:05:25 +0530

    Usecase 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MoodAnalyzer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Mood mood = new Mood();
+            string M= mood.Analyser();
+            Console.WriteLine(M);
+        }
+    }
+}
