a4fb93576bb92e1495805433603412fd66bfb333
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-27 13:06:47 +0530

    Check happy or sad mood

---
+++ Mood.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class Mood
+    {
+
+        public string Analyser()
+        {
+            Console.WriteLine("Enter you Mood: ");
+            string str =Console.ReadLine();
+            if (str == "i am in sad mood")
+                return "SAD";
+            else if (str == "i am in any mood")
+                return "HAPPY";
+            else
+                return "INVALID";
+        }
+    }
+}
